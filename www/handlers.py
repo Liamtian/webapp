ab6c3cce4267fc25a378e9c67faa8f48cd650ee2
Liam Tian <limtian@163.com>
2017-09-26 14:23:34 +0800

    add web api

--- www/handlers.py
+++ www/handlers.py
@@ -8,15 +8,6 @@ import re, time, json, logging, hashlib, base64, asyncio
 from coroweb import get, post
 from models import User, Comment, Blog, next_id
 
-'''
-@get('/')
-async def index(request):
-	users = await User.findAll()
-	return {
-		'__template__':'test.html',
-		'users': users
-	}
-'''
 
 @get('/')
 def index(request):
@@ -30,3 +21,10 @@ def index(request):
 		'__template__':'blogs.html',
 		'blogs': blogs
 	}
+
+@get('/api/users')
+async def api_get_users():
+	users = await User.findAll(orderBy='created_at desc')
+	for u in users:
+		u.passwd='******'
+	return dict(users=users)
